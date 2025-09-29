055a34574d600d51dc9a8731dcde09a549d04be0
bergenhem <cbergenhem@gmail.com>
2025-09-29 09:56:56 -0400

    Add getInitialProps function to MyApp (for Apollo)

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -11,7 +11,7 @@ Router.events.on('routeChangeError', () => NProgress.done());
 // TODO: Swap with our own
 import '../components/styles/nprogress.css';
 
-export default function MyApp({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   const apolloClient = createApolloClient(pageProps.initialApolloState);
   return (
     <ApolloProvider client={apolloClient}>
@@ -21,3 +21,16 @@ export default function MyApp({ Component, pageProps }) {
     </ApolloProvider>
   )
 };
+
+// If any pages have getInitialProps, then wait until the props have been fetches
+//   this is supposed to help with Apollo and Next.js
+MyApp.getInitialProps = async function ({ Component, ctx }) {
+  let pageProps = {};
+  if (Component.getInitialProps) {
+    pageProps = await Component.getInitialProps(ctx);
+  }
+  pageProps.query = ctx.query;
+  return { pageProps };
+};
+
+export default MyApp;
